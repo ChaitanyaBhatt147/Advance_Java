673cd390cd264c8b1c696ed1ac16b0a4dd97dfe7
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2025-09-17 17:58:28 +0530

    preparedStatement

---
+++ JDBC/src/com/rays/preparedStatement/TestUserModel.java
@@ -0,0 +1,10 @@
+package com.rays.preparedStatement;
+
+import java.sql.SQLException;
+
+public class TestUserModel {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserModel model = new UserModel();
+		System.out.println(model.nextPk());
+	}
+}
